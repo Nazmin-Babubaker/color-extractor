c7d0c0d1ae93f3a692f70f97dea8f0adbfac5cb5
Nazmin-Babubaker <nazminbabubakerr@gmail.com>
2026-04-05 19:45:27 +0530

    Refactor main function to include dominant color extraction and display in hex format

--- extractor.py
+++ extractor.py
@@ -20,10 +20,39 @@ def extract_palette(image_path):
     print(f"Total pixels to analyze: {len(pixels)}")
     return pixels
 
-def main():
-    print("Environment check: Success!")
-    extract_palette("image1.png")
+
+def get_dominant_colors(pixels, top_n=5):
     
+    color_counts = {}
+
+    for p in pixels:
+        
+        color = tuple(p)
+        
+        
+        if color in color_counts:
+            color_counts[color] += 1
+        else:
+            color_counts[color] = 1
+
+   
+    sorted_colors = sorted(color_counts.items(), key=lambda x: x[1], reverse=True)
+
+    
+    return sorted_colors[:top_n]
+
+
+def rgb_to_hex(rgb_tuple):
+    return '#{:02x}{:02x}{:02x}'.format(rgb_tuple[0], rgb_tuple[1], rgb_tuple[2]).upper()
+
+def main():
+  pixels = extract_palette("image1.png")
+  top_colors = get_dominant_colors(pixels)
+
+  print("--- Dominant Colors (Hex) ---")
+  for color, count in top_colors:
+    print(f"{rgb_to_hex(color)} : appeared {count} times")
+
 
 if __name__ == "__main__":
     main()
